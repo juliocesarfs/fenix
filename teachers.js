73962a40e1500ea4ebd3248e245f8adbbe18c773
Crucciatus <juliocesarafs2@gmail.com>
2020-07-16 17:45:37 -0300

    structuing data view

--- teachers.js
+++ teachers.js
@@ -9,7 +9,16 @@ exports.post = function(req, res) {
       return res.send('Please fill all fields');
   }
 
-  data.instructors.push(req.body);
+  let { avatar_url, name, birth, education_level, class_type, expertises } = req.body;
+
+  data.instructors.push({
+    avatar_url,
+    name,
+    birth,
+    education_level,
+    class_type,
+    expertises
+  });
 
   fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
     if (err) return res.send('Write file error!');
