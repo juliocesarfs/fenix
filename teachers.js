3254eb22921850515fe852b7882e855793712229
Crucciatus <juliocesarafs2@gmail.com>
2020-07-16 17:32:54 -0300

    adding data.json

--- teachers.js
+++ teachers.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const data = require('./data.json');
+
 exports.post = function(req, res) {
   const keys = Object.keys(req.body);
 
@@ -6,5 +9,11 @@ exports.post = function(req, res) {
       return res.send('Please fill all fields');
   }
 
-  return res.send(keys);
+  data.instructors.push(req.body);
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+    if (err) return res.send('Write file error!');
+
+    return res.redirect('teachers');
+  })
 }
