772d34083e1e34bef1734bd5780a4e75abe403fb
Crucciatus <juliocesarafs2@gmail.com>
2020-07-24 10:24:03 -0300

    create age function

--- utils.js
+++ utils.js
@@ -1,6 +1,18 @@
 const data = require('./data.json');
 
 module.exports = {
+  age: function(timestamp) {
+    const today = new Date();
+    const birthDate = new Date(timestamp);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const month = today.getMonth() - birthDate.getMonth();
+
+    if((month < 0 ) || (month == 0 && today.getDate() <= birthDate.getDate()))
+      age -= 1;
+
+    return age;
+  },
   findTeacher: function(params) {
     const { id } = params;
 
