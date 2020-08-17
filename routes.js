70b8b4c9a15beda964edd4cc4ac7b8717fe16839
Crucciatus <juliocesarafs2@gmail.com>
2020-08-17 15:15:53 -0300

    created DELETE TEACHER function

--- routes.js
+++ routes.js
@@ -22,6 +22,8 @@ routes.get('/teachers/:id/edit', teachers.edit);
 
 routes.put('/teachers', teachers.put);
 
+routes.delete('/teachers', teachers.delete);
+
 routes.get('/students', function(req, res) {
   return res.send('students');
 })
