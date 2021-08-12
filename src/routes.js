796bda5d051edd49f500c4e9a5a32f37e4e536e0
juliocesarfs <juliocesarafs2@gmail.com>
2021-08-12 19:07:44 -0300

    feat: add filter feature with query params in Teachers

--- src/routes.js
+++ src/routes.js
@@ -10,21 +10,21 @@ routes.get('/', function(req, res) {
 
 
 // TEACHERS
-routes.get('/teachers', teachers.index)
-routes.get('/teachers/create', teachers.create)
-routes.post('/teachers', teachers.post)
-routes.get('/teachers/:id', teachers.show)
-routes.get('/teachers/:id/edit', teachers.edit)
-routes.put('/teachers', teachers.put)
-routes.delete('/teachers', teachers.delete)
+routes.get('/teachers',           teachers.index)
+routes.get('/teachers/create',   teachers.create)
+routes.post('/teachers',           teachers.post)
+routes.get('/teachers/:id',        teachers.show)
+routes.get('/teachers/:id/edit',   teachers.edit)
+routes.put('/teachers',             teachers.put)
+routes.delete('/teachers',       teachers.delete)
 
 // STUDENTS
-routes.get('/students', students.index)
-routes.get('/students/create', students.create)
-routes.post('/students', students.post)
-routes.get('/students/:id', students.show)
-routes.get('/students/:id/edit', students.edit)
-routes.put('/students', students.put)
-routes.delete('/students', students.delete)
+routes.get('/students',           students.index)
+routes.get('/students/create',   students.create)
+routes.post('/students',           students.post)
+routes.get('/students/:id',        students.show)
+routes.get('/students/:id/edit',   students.edit)
+routes.put('/students',             students.put)
+routes.delete('/students',       students.delete)
 
 module.exports = routes;
